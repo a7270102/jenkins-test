ce7713b448e7edc238cbe0e0401aac4d0b96ab1e
a7270102 <a7270102@163.com>
2017-04-26 11:35:40 +0800

    xxx

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -21,16 +21,16 @@ describe('Hello.vue', () => {
   		.to.equal("property");
   });
 
-  // test for async method
-  it("should hello dataList correct", done => {
-  	const Constructor = Vue.extend(Hello);
-  	const vm = new Constructor().$mount();
-  	// 使用mock 去替换测试吧
-  	vm.getData();
-  	vm.$nextTick(() => {
-  		expect(vm.$data.dataList.length)
-  		.to.be.ok;
-  		done();
-  	})
-  });
+  // // test for async method; async is no problem
+  // it("should hello dataList correct", done => {
+  // 	const Constructor = Vue.extend(Hello);
+  // 	const vm = new Constructor().$mount();
+  // 	// 使用mock 去替换测试吧
+  // 	vm.getData();
+  // 	vm.$nextTick(() => {
+  // 		expect(vm.$data.dataList.length)
+  // 		.to.be.ok;
+  // 		done();
+  // 	})
+  // });
 })
